8726aabfc9767ab514b211eca82ad984d67b0acd
arek-jozwiak-coderslab <Gonsie123>
2020-08-17 11:15:34 +0200

    gitignore

---
+++ src/main/java/pl/coderslab/model/Book.java
@@ -0,0 +1,28 @@
+package pl.coderslab.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
